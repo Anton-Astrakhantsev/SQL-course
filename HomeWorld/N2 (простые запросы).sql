a176493698362ebe222b330415584f9c162b425a
Anton-Astrakhantsev <36815626+Anton-Astrakhantsev@users.noreply.github.com>
2019-01-30 15:55:37 +0300

    Update N2 (простые запросы).sql

--- HomeWorld/N2 (простые запросы).sql
+++ HomeWorld/N2 (простые запросы).sql
@@ -4,30 +4,30 @@ select 'Антон Астраханцев' as fio;
 select 'N1' as query;
 
 select * from ratings limit 10;
-select * from links where imdbid like '%42' and movieid between 100 and 1000 limit 10;
+select * from links where imdbId like '%42' and movieId between 100 and 1000 limit 10;
 
 
 select 'N2' as query;
 
-select distinct imdbid
+select distinct imdbId
 from links as l
 inner join ratings as r
-on (l.movieid=r.movieid)
+on (l.movieId=r.movieId)
 where r.rating = 5
 limit 10;
 
 
 select 'N3' as query;
 
-select count(distinct l.movieid)
+select count(distinct l.movieId)
 from links as l
 left join ratings as r
-on (l.movieid=r.movieid)
-where r.movieid is null;
+on (l.movieId=r.movieId)
+where r.movieId is null;
 
-select userid
+select userId
 from ratings
-group by userid
+group by userId
 having avg(rating) > 3.5
 order by avg(rating) desc
 limit 10;
@@ -35,21 +35,21 @@ limit 10;
 
 select 'N4' as query;
 
-select imdbid
+select imdbId
 from links as l
 left join ratings as r
-on (l.movieid=r.movieid)
-where l.movieid in (
-select movieid
+on (l.movieId=r.movieId)
+where l.movieId in (
+select movieId
 from ratings
-group by movieid
+group by movieId
 having avg(rating) > 3.5)
 limit 10;
 
 with eleven as(
-select userid, count(rating)
+select userId, count(rating)
 from ratings
-group by userid
+group by userId
 having count(rating) > 10)
 select round(avg(rating), 3)
-from ratings inner join eleven using (userid);
+from ratings inner join eleven using (userId);
